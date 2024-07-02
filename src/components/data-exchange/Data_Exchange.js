c622c8f6a17a61631e18f9130aacb13f396b5e84
Eugenia Pramesti Simabrata <eugenia301003@gmail.com>
2024-07-02 11:39:45 +0700

    change in url

--- src/components/data-exchange/Data_Exchange.js
+++ src/components/data-exchange/Data_Exchange.js
@@ -5,7 +5,7 @@ import axios from 'axios'; // Import Axios
 import { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 
-const url1 = 'http://52.118.165.131:8443';
+const url1 = 'http://52.118.170.239:8443';
 const url2 = 'http://127.0.0.1:5000'
 
 function Data_Exchange() {
