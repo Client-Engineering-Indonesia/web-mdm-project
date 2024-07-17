01990d576fe4837c384eb8f9c595d12b38016015
Jose Pereira <Jose.Pereira1@ibm.com>
2024-07-17 10:45:53 +0700

    Added endpoint for form submission, not tested ye

--- halo.py
+++ halo.py
@@ -560,19 +560,16 @@ def add_new_role():
 def assign_role(username):
     try:
         data = request.get_json()
-        print(data)
+
         if not data:
             return jsonify({'error': 'No data provided'}), 400
-
+        print(data)
         user_roles = data.get('user_roles')
-
-        if not username or not user_roles:
+        webToken = data.get('webtoken')
+        if not username or not user_roles or not webToken: 
             return jsonify({'error': 'Missing required parameters'}), 400
-
-        # auth_header = request.headers.get('Authorization')
-        # if not auth_header:
-        #     return jsonify({'error': 'Authorization header missing'}), 401
-        user_info = decodeJwtToken(data.get("web_token"))
+        user_info = decodeJwtToken(data.get('webtoken'))
+        print(user_info)
         token = user_info["cp4d_token"]
         url = f'{cp4d_url}/usermgmt/v1/user/{username}'
 
@@ -589,13 +586,13 @@ def assign_role(username):
         response = requests.put(url, headers=headers, json=payload, verify=False)
 
         if response.status_code == 200:
-            return jsonify({'message': 'ok'}), 200
+            return jsonify({'message': response.json()}), 200
         else:
             return jsonify({'error': 'failed', 'details': response.text}), response.status_code
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
+        
 # Get User Info
 @app.route('/user_info', methods=['POST'])
 def get_user_info_from_jwt():
